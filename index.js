90e096a0618562be5e52f6ecfef7e2f511832745
Kristian Semjonov <kristian.semjonov@gmail.com>
2021-03-12 11:26:55 +0200

    emoji added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
   if (randomNumber1 === randomNumber2) {
     document.querySelector("h1").innerHTML = "DRAW! Roll again!";
   } else if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 1 WINS!"
+    document.querySelector("h1").innerHTML = "🔥 Player 1 WINS!"
   } else {
-    document.querySelector("h1").innerHTML = "Player 2 WINS!"
+    document.querySelector("h1").innerHTML = "Player 2 WINS! 🔥"
   }
